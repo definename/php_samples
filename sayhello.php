75c269abb768983081fc0bf793ebdd9568e64a38
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-12-08 22:55:55 +0200

    Array key.

--- sayhello.php
+++ sayhello.php
@@ -1,8 +1,8 @@
 <?php
     $user = $_POST["user"];
 
-    $hello_array[] = nl2br("Hello0, " . $user);
-    $hello_array[] = nl2br("Hello1, " . $user);
+    $hello_array["one"] = nl2br("Hello0, " . $user);
+    $hello_array["two"] = nl2br("Hello1, " . $user);
 
     print "<table border='1'>\n";
     foreach ($hello_array as $key => $value) {
