e380db6bda4730569e1307420ddacf2520365244
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-12-15 18:42:30 +0200

    php globals.

--- sayhello.php
+++ sayhello.php
@@ -4,18 +4,21 @@ function page_header($title="Welcome")
 {
     print "<html><head><title>$title</title></head>";
     print "<body bgcolor='c0c0c0'>";
+    $user = $GLOBALS["user"];
+    print "Hello $user nice to mite you";
+    print "<hr>";
 }
 
 function page_foter()
 {
-    print "<hr>Thank you for visit";
+    $user = $GLOBALS["user"];
+    print "<hr> $user thank you for visit";
     print "</body></html>";
 }
 
-page_header();
-
 $user = $_POST["user"];
-print nl2br("Hello " . $user . " here is summury for you\n\n");
+
+page_header();
 
 $state_city_population = array("New York" => array("New York" => 8175133),
                                 "California" => array("Los Angeles" => 3792621,
