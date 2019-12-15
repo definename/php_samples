66819795fddcab622383f18d2a8c1fae1c6a50de
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2019-12-15 23:31:00 +0200

    Inheritance, namespace.

--- dish.php
+++ dish.php
@@ -1,12 +1,38 @@
 <?php
+    namespace Order;
+
     class Dish
     {
-        public $name;
-        public $ingredients = array();
+        private $name;
+        protected $ingredients = array();
+
+        public function __construct(string $name, array $ingredients)
+        {
+            $this->name = $name;
+            $this->ingredients = $ingredients;
+        }
+
         public function has_ingredient(string $ing) : bool
         {
             $ret = in_array($ing, $this->ingredients);
             return $ret;
         }
     }
-?>
+
+    class ComboDish extends Dish
+    {
+        public function __construct(string $name, array $ingredients)
+        {
+            parent::__construct($name, $ingredients);
+        }
+
+        public function has_ingredient(string $ing) : bool
+        {
+            foreach ($this->ingredients as $dish) {
+                if ($dish->has_ingredient($ing)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
--- index.php
+++ index.php
@@ -11,21 +11,30 @@
     <button form="say" type="submit">Submit</button>
 
     <?php
+
+    use \Order\Dish;
+    use \Order\ComboDish;
+
     print nl2br("<hr>Class:\n");
     require "dish.php";
 
-    $soup = new Dish;
-    $soup->name = "Chiken soup";
-    $soup->ingredients[] = "Chiken";
-    $soup->ingredients[] = "Water";
+    $ingredients = ["Water", "Chiken", "Pickles"];
 
-    foreach (["Water", "Chiken", "Carrot"] as $ing) {
+    $soup = new Dish("Soup", array("Chiken", "Water"));
+    foreach ($ingredients as $ing) {
         if ($soup->has_ingredient($ing)) {
-            print nl2br("+Soup contains $ing\n");
-        } else {
-            print nl2br("-Soup does not contain $ing\n");
+            print nl2br("Soup contains $ing\n");
         }
     }
+
+    $sandwich = new Dish("Sandwich", array("Bread", "Chiken"));
+    $combo = new ComboDish("Soup + Sandwich", [$soup, $sandwich]);
+    foreach ($ingredients as $ing) {
+        if ($combo->has_ingredient($ing)) {
+            print nl2br("Something in combo contains $ing\n");
+        }
+    }
+
 ?>
 
 </body>
